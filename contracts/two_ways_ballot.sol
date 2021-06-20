e5385dc31691bcafd4b7f3cbc44f498953d10360
JacopoBugini <jacopo.bugini@gmail.com>
2021-06-20 16:50:36 +0200

    feat: optimized code and organized part 2

---
+++ contracts/two_ways_ballot.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8;
+contract Ballot{
+    
+    struct Voter{
+        uint voter_points; // totally z points
+        bool voted; // whether or not voted
+        string name; // name of voter
+        string[] preferred_option_names; //voted to which options
+    }
+    struct Leader{
+        string option_name; //corresponding option
+        uint leader_points;//total y points
+        bool assigned; //whether or not assigned to voters
+        address leader_addr;
+        address[] assign_voters; //assign to which voters
+    }
+
+    struct Option{
+        string name; //name of option
+        uint count; //number of accumulated votes
+    }
+
+    
+    struct vv{
+        string name;
+        uint count;
+    }
+
+    mapping (string=> mapping (string=>Option)) public voter_option_match; //mapping between voters and options
+    mapping (string=> mapping (string=>vv)) public option_voter_match;
+
+    uint256 public deadline; //deadline of the voting
+    address public chairperson; //promoter
+    mapping(address=>Voter) public voters; //voters
+    mapping(address=>Leader) public Leaders; 
+    uint public z; //fixed points assigned to each voter
+    uint public y; //fixed points assigned to each project(option) leader
+    string[] public Options; // store name of options
+    address[] public Valid_voter_address; //separately store all the addresses of authorized voters
+    address[] public leader_address;
+    uint public num_options=0; //record the number of options
+    uint public num_voters=0; //record the number of voters
+
+    
+    
+    //new ballot
+    constructor(string[] memory optionNames,address[] memory leaders, uint points_per_voter,uint points_per_leader){
+        chairperson=msg.sender; 
+        //initial setting of Options
+        for (uint i=0;i<optionNames.length;i++){
+            Options.push(optionNames[i]); //insert name
+            num_options++;
+        }
+        z=points_per_voter;
+        y=points_per_leader;
+        //initialize the leaders info
+        for (uint i=0;i<leaders.length;i++){
+            Leaders[leaders[i]].option_name=optionNames[i];
+            Leaders[leaders[i]].leader_points=y;
+            Leaders[leaders[i]].assigned=false;
+            Leaders[leaders[i]].leader_addr=leaders[i];
+
+            leader_address.push(leaders[i]); //store the leader address separately
+        }
+
+        deadline=block.timestamp+240*60*60; //once the voters have votes, the countdown starts, voters have to accomplish voting within one day.
+
+    }
+   
+    function Give_right_to_voters(address voter, string memory name) public{
+        require(msg.sender==chairperson); //must done by chairperson
+        require(!voters[voter].voted); 
+        require(voters[voter].voter_points==0); 
+        voters[voter].voter_points=z; //give voter N points for this new ballot
+        voters[voter].name=name;
+
+        Valid_voter_address.push(voter); //store the address
+        num_voters++;
+
+        //initialize the voter_group_match---for each voter, initally set vote count=0 for all options
+        for(uint i=0;i<num_options;i++){
+            voter_option_match[voters[voter].name][Options[i]].name=Options[i];
+            voter_option_match[voters[voter].name][Options[i]].count=0;
+        }
+
+        for(uint i=0;i<num_options;i++){
+            option_voter_match[Options[i]][voters[voter].name].name=voters[voter].name;
+            option_voter_match[Options[i]][voters[voter].name].count=0;
+        }
+    }
+    
+    //do not use any state variable, so can be resitricted to pure
+    function Sum(uint[] memory point_allocation) public pure returns(uint){
+        uint sum=0;
+        for (uint i=0;i<point_allocation.length;i++){
+            sum+=point_allocation[i];
+            }
+        return sum;
+    }
+
+    function compare_str(string memory str1,string memory str2) public pure returns(bool){
+        return keccak256(abi.encodePacked(str1))==keccak256(abi.encodePacked(str2));}
+    
+    function Vote(string[] memory option_names,uint[] memory point_allocation) public{
+        //option_idx: voter's decision about to choose which options
+        //point_allocation: how to allocation the N points to above options
+        
+        Voter storage v=voters[msg.sender]; // voter object
+
+        require(block.timestamp<deadline,"Voting has finished!");//should vote before deadline
+        require(option_names.length==point_allocation.length);
+        for(uint i=0;i<option_names.length;i++){
+            bool within=false;
+            for (uint j=0;j<Options.length;j++){
+                if (compare_str(option_names[i],Options[j])==true){
+                    within=true;
+                    break;
+                }
+            }
+            require(within==true,"Non-existing option!");
+        }
+        //check whether the option_idx beyond the max possible option idx
+        require(Sum(point_allocation)==v.voter_points,"Sum of your votes should be equal to z!"); 
+        //the sum of voter's point allocation must equal points assigned to him/her
+        require(!v.voted,"You have already voted!"); 
+        v.voted=true;
+        v.preferred_option_names=option_names;
+
+        for(uint i=0;i<option_names.length;i++){
+           // Need to split points before storing
+           voter_option_match[v.name][option_names[i]].count+=point_allocation[i];
+           v.preferred_option_names.push(option_names[i]);
+        }
+    }
+
+    function Vote_leader(address[] memory prefer_voters,uint[] memory point_assignment) public {
+        Leader storage l=Leaders[msg.sender];
+        
+        require(block.timestamp<deadline,"Time is over!");//should assign before deadline
+        require(prefer_voters.length==point_assignment.length);
+        for(uint i=0;i<prefer_voters.length;i++){
+            bool valid_voter=false;
+            for (uint j=0;j<Valid_voter_address.length;j++){
+                if (prefer_voters[i]==Valid_voter_address[j]){
+                    valid_voter=true;
+                    break;
+                }
+            }
+            require(valid_voter=true,"Non-existing voter!");
+        }
+        require(Sum(point_assignment)==l.leader_points,"Sum of your assign points should be equal to y!");
+        require(!l.assigned,"You have already assigned!");
+        l.assigned=true;
+        l.assign_voters=prefer_voters;
+
+        for(uint i=0;i<prefer_voters.length;i++){
+            option_voter_match[l.option_name][voters[prefer_voters[i]].name].count+=point_assignment[i];
+        }
+    
+    }  
+
+}
---
+++ flask_simple_ballot.py
@@ -0,0 +1,101 @@
+from utils.ass_vote_utils import one_way_ass_vote
+from flask import Flask,render_template,request,redirect,send_from_directory
+from collections import defaultdict
+from web3 import Web3
+import json
+import time
+import os
+import sys
+
+# Initialize variables
+contract_addr=sys.argv[1]
+w3,vote_contract = one_way_ass_vote(contract_addr)
+vote_app=Flask(__name__,static_folder='templates/static',template_folder='templates/one_way_ballot')
+
+# Run Application
+@vote_app.route('/')
+def welcome():
+    return render_template('/content.html')
+
+@vote_app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(vote_app.root_path,'static'),'favicon.ico')
+
+@vote_app.route('/<filename>')
+def ballot_status(filename):
+    num_options=vote_contract.functions.num_options().call()
+    status_data=[vote_contract.functions.Options(i).call() for i in range(num_options)]
+    # with open('static/data/ballot_status.json','w') as f:
+    #     json.dump(status_data,f)
+    return render_template(filename,update=status_data)
+
+@vote_app.route('/<filename>',methods=['POST','GET'])
+def login(filename):
+
+    if request.method=='GET':
+       return render_template(filename,error_message='')
+    #POST
+    addr=request.form.get('Address')
+
+    #all authorized voters' addresses
+    num_voters=vote_contract.functions.num_voters().call()
+    authorized_addrs=[vote_contract.functions.Valid_voter_address(i).call() for i in range(num_voters)]
+    
+    if addr not in authorized_addrs:
+        return render_template(filename,error_message='You have no right to vote!')
+    else:
+        current_voter=vote_contract.functions.voters(addr).call()
+        if current_voter[1]==True:
+            return render_template(filename,error_message='You have already voted!')
+        elif time.time()>vote_contract.functions.deadline().call():
+            return render_template(filename,error_message='The ballout is over!')
+        else:
+            redirect_addr='/vote/'+addr
+            return redirect(redirect_addr)
+
+@vote_app.route('/vote/<addr>',methods=['POST','GET'])
+def vote(addr):
+    num_options=vote_contract.functions.num_options().call()
+    status_data=[vote_contract.functions.Options(i).call() for i in range(num_options)]
+    if request.method=='GET':
+        return render_template('vote.html',data=status_data,error_message='')
+    #receive post
+    vote_info={} 
+    for i,(opt,_) in enumerate(status_data):
+        point=request.form.get(opt)
+        if point:
+            if not point.isdigit():
+                return render_template('vote.html',data=status_data,error_message='Wrong input type,Only integers are accepted!')
+            elif int(point)>0:
+                vote_info[i]=int(point)
+    
+    option_idx=list(map(lambda x:int(x),vote_info.keys()))
+    point_allocation=list(vote_info.values())
+
+ 
+    voter_info=vote_contract.functions.voters(addr).call()
+    num_points_given=voter_info[0]
+    if sum(point_allocation)!=num_points_given:
+        return render_template('vote.html',data=status_data,error_message='You have {} points totally!'.format(num_points_given))
+    else:
+        vote_yet=voter_info[1]
+        if vote_yet:
+            return render_template('vote.html',data=status_data,error_message='You have already voted!')
+        vote_contract.functions.Vote(option_idx=option_idx,point_allocation=point_allocation).\
+            transact(transaction={"from":addr})
+        
+        return render_template('vote_done.html')
+
+if __name__=='__main__':
+    vote_app.run()  
+    
+    
+    
+
+
+    
+    
+    
+    
+    
+    
